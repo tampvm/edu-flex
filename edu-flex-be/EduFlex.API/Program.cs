f7610949ebc0650926802abc63f62f3f465e7813
i'm tum <minhtam250102@gmail.com>
2024-09-15 08:26:01 +0700

    add dependency injection service to project and AppDbContext

--- edu-flex-be/EduFlex.API/Program.cs
+++ edu-flex-be/EduFlex.API/Program.cs
@@ -1,11 +1,12 @@
+using EduFlex.Service.Common;
+using Elepla.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var configuration = builder.Configuration.Get<AppConfiguration>();
+builder.Services.AddInfrastructuresService(configuration.DatabaseConnection);
+builder.Services.AddWebAPIService();
+builder.Services.AddSingleton(configuration);
 
 var app = builder.Build();
 
---
+++ edu-flex-be/EduFlex.Domain/Enums/GenderEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduFlex.Domain.Enums
+{
+    public enum GenderEnums
+    {
+        Unknown,
+        Male,
+        Female
+    }
+}
