3161c537f5c454d811256f0d9f94982036661815
i'm tum <minhtam250102@gmail.com>
2024-11-08 20:01:49 +0700

    set up project

--- edu-flex-be/EduFlex.API/Program.cs
+++ edu-flex-be/EduFlex.API/Program.cs
@@ -3,6 +3,10 @@ using Elepla.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+
 var configuration = builder.Configuration.Get<AppConfiguration>();
 builder.Services.AddInfrastructuresService(configuration.DatabaseConnection);
 builder.Services.AddWebAPIService();
@@ -16,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHealthChecks("/health");
 
 app.UseHttpsRedirection();
 
