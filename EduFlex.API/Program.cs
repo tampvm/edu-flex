676409a0939c2068e2b6bb0e9d5bcffc378ee81b
i'm tum <minhtam250102@gmail.com>
2024-11-09 02:13:29 +0700

    set up project

--- EduFlex.API/Program.cs
+++ EduFlex.API/Program.cs
@@ -16,11 +16,14 @@ builder.Services.AddSingleton(configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "EduFlex API");
+    options.RoutePrefix = string.Empty;
+});
+
 
 app.UseHttpsRedirection();
 
